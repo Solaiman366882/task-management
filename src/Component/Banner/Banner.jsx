01bd12029cfad09087dd1c8f10597563fca5a415
Md.Solaiman <md.solaiman366882@gmail.com>
2023-12-22 03:23:59 +0600

    login page design completed

--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -1,20 +1,30 @@
+import { Link } from "react-router-dom";
 import "./Banner.css";
 const Banner = () => {
-    return (
-        <section className="banner-section">
-            <div className="max-w-screen-xl mx-auto p-5 h-full">
-                <div className="hero w-full flex min-h-[80vh] flex-col justify-center">
-                    <h1 className="text-[#FF6D3C] font-bold text-6xl ">M Task</h1>
-                    <p className="mt-3 mb-6 font-medium text-lg">Manage your task or work, and track them to <br /> increase your productivity. Its easy to operate ...</p>
-                    <div>
-                        <div className="">
-                            <button className="btn">{"Let's Explore"}</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+	return (
+		<section className="banner-section">
+			<div className="max-w-screen-xl mx-auto p-5 h-full">
+				<div className="hero w-full flex min-h-[80vh] flex-col justify-center">
+					<h1 className="text-[#FF6D3C] font-bold text-6xl">
+						M Task
+					</h1>
+					<p className="mt-3 mb-6 font-medium text-lg">
+						Manage your task or work, and track them to <br />{" "}
+						increase your productivity. Its easy to operate ...
+					</p>
+					<div>
+						<div className="">
+							<Link to="/login">
+								<button className="btn">
+									{"Let's Explore"}
+								</button>
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Banner;
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,10 +1,55 @@
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-    return (
-        <div>
-            this is login
-        </div>
-    );
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
+
+	const handleLogin = (data) => {
+		console.log(data);
+	};
+
+	return (
+		<div className="max-w-screen-md mx-auto px-5">
+            <h2 className="mt-8 text-[#FF6D3C] font-bold text-5xl text-center">Login Here</h2>
+			<form
+				onSubmit={handleSubmit(handleLogin)}
+				className="flex flex-col gap-5 p-10 border-2 my-12 border-rose-950"
+			>
+				<div className="flex-1">
+					<input
+						//defaultValue="John Doe"
+						{...register("email", { required: true })}
+						className="h-10 w-full border border-[#FF6D3C] pl-5"
+						placeholder="Enter Your Email"
+					/>
+					{errors.email && (
+						<span className="text-red-700 block font-medium">
+							Email is required
+						</span>
+					)}
+				</div>
+
+				<div className="flex-1">
+					<input
+						{...register("password", { required: true })}
+						className="h-10 w-full border border-[#FF6D3C] pl-5"
+						placeholder="Enter Your password"
+					/>
+					{/* errors will return when field validation fails  */}
+					{errors.password && (
+						<span className="text-red-700 block font-medium">
+							Password is required
+						</span>
+					)}
+				</div>
+
+				<input type="submit" className="btn" />
+			</form>
+		</div>
+	);
 };
 
 export default Login;
